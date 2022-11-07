d1e258720f313fb3205d754b9b6710d706a89e09
Vincent Sadili <111635064+dkoalam@users.noreply.github.com>
2022-11-07 23:53:53 +0800

    Add files via upload

---
+++ SetA_array.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Scanner n = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String yName=s.nextLine();
+
+
+        System.out.print("Enter number of Student you want to input: ");
+        int numStudent=n.nextInt();
+       //var name Array = new var[length/input]
+        int grades[]=new int[numStudent];
+        String name[]=new String [numStudent];
+
+        int pg=0,fg=0;
+      //     start in 0;end i<input 
+        for (int i=0;i<numStudent;i++){
+
+            System.out.println("Enter Student name:");
+            name[i]=s.nextLine();
+
+            System.out.print("Enter Grades: ");
+            grades[i]=n.nextInt();
+
+            if(grades[i]<75 && grades[i]>0){
+                System.out.println("GRADE FAILED!");
+                fg++;
+
+            }
+            else if(grades[i]>=75 && grades[i]<100){
+                System.out.println("GRADE PASSED!");
+                pg++;
+            }
+            else{
+                System.out.println("Invalid input");
+                System.exit(0);
+            }
+}
+        for (int i=0;i<numStudent;i++){
+            System.out.println((i+1)+"."+name[i]+"-"+grades[i]);
+        }
+
+        System.out.println("Hi Prof! "+yName);
+        System.out.println("There are "+pg+" passed and "+fg+" failed among the "+numStudent+" student grades.");
+
+        if(pg>fg){
+            System.out.println("CONGRATULATIONS MAJORITY OF THE STUDENTS PASSED!");
+
+        }
+        else{
+            System.out.println("SADLY MAJORITY OF THE STUDENTS FAILED!");
+        }
+
+
+
+
+
+
+        
+
+
+
+    
+
+
+    }
+}
