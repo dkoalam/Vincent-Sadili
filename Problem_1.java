ad83d7abed982467c22fe3a409c62cf508fb3d3b
Vincent Sadili <111635064+dkoalam@users.noreply.github.com>
2022-11-08 23:12:01 +0800

    Add files via upload

---
+++ Problem_1.java
@@ -0,0 +1,47 @@
+//vaSadili_inf222
+import java.util.Scanner;
+
+public class Problem_1 {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+//get input
+        System.out.print("Enter the number of frequent flyers: ");
+        int numF=s.nextInt();
+
+        int flyers[]=new int[numF];
+        System.out.print("Enter the frequent flyers: ");
+        //Enter the frequent flyers: 1 1 3 4 2 one space between inputs
+        int i =0;
+        for(i=0;i<flyers.length;i++){
+            flyers[i]=s.nextInt();
+        }
+int mFrequent=0;
+int count=0;
+        //get the most frequent
+        for(int j=0;j<flyers.length;j++){
+            int tempFlyers=flyers[j];
+            int tempCount=0;
+            if(tempFlyers<=100 && tempFlyers>=1){
+               for(int p=0;p<flyers.length;p++){
+                if(flyers[p]==tempFlyers){
+                    tempCount++;
+                }
+                }
+                if(tempCount>count){
+                    mFrequent=tempFlyers;
+                    count=tempCount;
+                }
+               
+             
+           }
+           else{
+            System.out.println("Invalid input");
+            System.out.println("Each frequent flyer number is within the range: <= 100 and >= 1");
+            System.exit(0);
+           }
+             
+        }
+        
+      System.out.println("Most frequent = "+mFrequent);
+    }
+}
---
+++ Problem_2.java
@@ -0,0 +1,69 @@
+//vaSadili_inf222
+import java.util.Scanner;
+
+public class Problem_2 {
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);
+
+        System.out.println("Bills: 1000, 500, 200, 100, 50, 20");
+        System.out.println("Coins: 10, 5, 1");
+
+        System.out.print("Enter the amount:");
+        double amount1=s.nextDouble();
+        int amount=(int)amount1;
+
+        if(amount>0){
+        int rem=amount%1000;
+        int countThousands=amount/1000;
+        System.out.println("P1000 bills: "+ countThousands);
+
+        //computation
+        int remfor500=rem%500;
+        int count500=rem/500;
+        System.out.println("P500 bills: "+count500);
+
+        int remfor200=remfor500%200;
+        int count200=remfor500/200;
+        System.out.println("P200 bills: "+count200);
+
+        int remfor100=remfor200%100;
+        int count100=remfor200/100;
+        System.out.println("P100 bills: "+count100);
+
+        int remfor50=remfor100%50;
+        int count50=remfor100/50;
+        System.out.println("P50 bills: "+count50);
+
+        int remfor20=remfor50%20;
+        int count20=remfor50/20;
+        System.out.println("P20 bills: "+count20);
+
+        int remfor10=remfor20%10;
+        int count10=remfor20/10;
+        System.out.println("P10 coins: "+count10);
+
+        int remfor5=remfor10%5;
+        int count5=remfor10/5;
+        System.out.println("P5 coins: "+count5);
+
+        int remfor1=remfor5%1;
+        int count1=remfor5/1;
+        System.out.println("P1 coins: "+count1);   
+        }
+        else{
+            System.out.println("Invalid input");
+           
+        }
+
+ 
+
+
+
+
+  
+
+
+        
+    }
+}
