c2650bf6014bc5d0b9897ab07a26e2592a087fc0
Vincent Sadili <111635064+dkoalam@users.noreply.github.com>
2022-11-09 12:07:19 +0800

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -55,6 +55,38 @@ public class Problem2 {
             System.out.println("Invalid input");
 
         }
+/*
+Problem 2 Description
 
+We plan on making a new ATM machine that would dispense the exact amount you desire. The best part is, we plan on giving the least number of bills and coins possible! Creating the software for it would be the perfect job for someone as talented as you are. Do this, and I guarantee you we will be rich in no time!
+
+Denominations:
+
+Bills: 1000, 500, 200, 100, 50, 20
+
+Coins: 10, 5, 1
+
+Test Cases:
+
+Enter the amount: 1575.77
+
+P1000 bills: 1
+
+P500 bills: 1
+
+P200 bills: 0
+
+P100 bills: 0
+
+P50 bills: 1
+
+P20 bills: 1
+
+P10 coins: 0
+
+P5 coins: 1
+
+P1 coins: 0
+*/
     }
 }
