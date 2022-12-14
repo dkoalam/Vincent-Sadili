53e617efe988c0434354fac319bbd7d192c599dc
Vincent Sadili <111635064+dkoalam@users.noreply.github.com>
2022-12-14 11:12:21 +0800

    Add files via upload

---
+++ vincent.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class vincent{
+public static void main(String[] args) {
+    Scanner s =new Scanner(System.in);
+System.out.println("num1");
+int a=s.nextInt();
+System.out.println("num2");
+int b=s.nextInt();
+System.out.println("operation");
+String p=s.next();
+
+name();
+
+
+opera(a,b,p);
+
+//System.out.println(getnum());
+
+
+}
+
+public static void name() {
+    System.out.println("hi again");
+}
+public static void opera(int a,int b,String op) {
+    if(op.equalsIgnoreCase("ADD")){
+           System.out.println(a+b);
+    }else if(op.equalsIgnoreCase("sub")){
+        System.out.println(a-b);
+    }else if(op.equalsIgnoreCase("multi")){
+        System.out.println(a*b);
+    }else if(op.equalsIgnoreCase("div")){
+        System.out.println(a/b);
+    }else System.out.println("error");
+ 
+}
+
+public static int getnum(){
+    return 10;
+}
+
+
+}
